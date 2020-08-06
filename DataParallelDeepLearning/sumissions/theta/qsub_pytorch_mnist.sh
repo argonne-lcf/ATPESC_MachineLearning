6297038464b07e9a2a598eeaefa88e60b11d5326
Huihuo Zheng <zhenghh04@gmail.com>
2020-08-06 10:34:47 -0500

    added DataParallelDeepLearning

---
+++ DataParallelDeepLearning/sumissions/theta/qsub_pytorch_mnist.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#COBALT -n 1 
+#COBALT -t 1:00:00
+#COBALT -q training --attrs mcdram=cache:numa=quad
+#COBALT -A SDL_Workshop
+
+#submisstion script for running tensorflow_mnist with horovod
+
+echo "Running Cobalt Job $COBALT_JOBID."
+
+#Loading modules
+module load datascience/pytorch-1.1
+
+PROC_PER_NODE=4
+
+aprun -n $(($COBALT_JOBSIZE*$PROC_PER_NODE)) -N $PROC_PER_NODE \
+    -j 2 -d 32 -cc depth \
+    -e OMP_NUM_THREADS=32 \
+    -e KMP_BLOCKTIME=0 \
+    python pytorch_mnist.py --num_threads=32 --device cpu
+
