c3cc22432fbe3adbd639ba72078aec6a7c05f7c0
Taylor Childers <taylorchilders@gmail.com>
2024-08-01 13:26:31 -0500

    Updates to use DDP

--- 02_dataPipelines/01_pytorchDatasetAPI/ilsvrc_dataset.py
+++ 02_dataPipelines/01_pytorchDatasetAPI/ilsvrc_dataset.py
@@ -15,29 +15,31 @@
 #
 # questions? Taylor Childers, jchilders@anl.gov
 
+import logging
+import os
+import glob
+import time
+import sys
+import json
 from PIL import Image as PIL_Image
 import torch
 import torchvision
-import logging,os,glob,time,sys
 import numpy as np
 import xml.etree.ElementTree as ET
-import horovod.torch as hvd
+from argparse import ArgumentParser
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data.distributed import DistributedSampler
+
 logger = logging.getLogger(__name__)
-hvd.init()
-
-# these are initialized in the get_datasets function and used later
-labels_hash = None
-crop_size = None
-
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self,filelist,config):
-        self.config         = config
-        self.resize_shape   = config['data']['crop_image_size']
-        self.filelist       = filelist
-        self.len            = len(self.filelist)
-
-        self.labels_hash    = self.get_label_tables(config['data']['train_filelist'])
+    def __init__(self, filelist, config):
+        self.config = config
+        self.resize_shape = config['data']['crop_image_size']
+        self.filelist = filelist
+        self.len = len(self.filelist)
+        self.labels_hash = self.get_label_tables(config['data']['train_filelist'])
 
     ## Create a hash table for labels from string to int 
     @staticmethod
@@ -62,40 +64,27 @@ class Dataset(torch.utils.data.Dataset):
         labels = [os.path.basename(i) for i in labels]
         
         # create map from text to number
-        labels_hash = {}
-        for i,label in enumerate(labels):
-            labels_hash[label] = i
-
+        labels_hash = {label: i for i, label in enumerate(labels)}
         return labels_hash
 
     @classmethod
-    def from_filelist(cls,filelist_filename,config):
-        filelist = []
+    def from_filelist(cls, filelist_filename, config):
         with open(filelist_filename) as file:
-            for line in file:
-                filelist.append(line.strip())
+            filelist = [line.strip() for line in file if line.strip()]
+        return cls(filelist, config)
 
-        if filelist[-1] == '':
-            filelist.pop()
-        return cls(filelist,config)
-
-    def __getitem__(self,index):
+    def __getitem__(self, index):
         filename = self.filelist[index]
-        
         imgs, labels = self.read_jpeg(filename)
-
         return imgs, labels
 
     def __len__(self):
         return self.len
 
-    def read_jpeg(self,filename):
-
+    def read_jpeg(self, filename):
         try:
             label = filename.split('/')[-2]
-            
-            annotation_filename = filename.replace('Data','Annotations').replace('JPEG','xml')
-
+            annotation_filename = filename.replace('Data', 'Annotations').replace('JPEG', 'xml')
             # open the JPEG
             img = PIL_Image.open(filename).convert(mode='RGB')
             # convert to a tensor with NCHW
@@ -106,37 +95,34 @@ class Dataset(torch.utils.data.Dataset):
             
             # crop based on bonuding boxes and resize to same size
             if bounding_boxes is not None:
-                img = self.crop_and_resize(img,bounding_boxes,self.resize_shape)
+                imgs = self.crop_and_resize(img, bounding_boxes, self.resize_shape)
+                # only use the first image
+                img = imgs[0]
             else:
                 img = torchvision.transforms.Resize(self.resize_shape)(img)
                 img = torchvision.transforms.functional.to_tensor(img)
-
-            imgs = torch.unsqueeze(img,0)
-
+            
+            imgs = torch.unsqueeze(img, 0)
+            
             # convert string label to numerical label
             label = self.labels_hash[label]
             # duplicate labels to match the number of images created from bounding boxes
-            labels = torch.full([imgs.shape[0]],label)
+            labels = torch.full([imgs.shape[0]], label)
             # return images and labels
-            # logger.info('img = %s  imgs = %s',img.shape,imgs.shape)
             return imgs, labels
-        except:
-            logger.exception('filename: %s',filename)
+        except Exception as e:
+            logger.exception(f'Error processing file {filename}: {e}')
             raise
 
-
     @staticmethod
-    def crop_and_resize(image,bounding_boxes,resize_shape):
-        timg = torchvision.transforms.functional.to_tensor(image)
-        imgs = torch.zeros(len(bounding_boxes),timg.shape[0],resize_shape[0],resize_shape[1])
-        for i in range(bounding_boxes.shape[0]):
-            bb = bounding_boxes[i]
-            #img = image[...,bb[0]:bb[2],bb[1]:bb[3]]
-            timg = torchvision.transforms.Resize(resize_shape)(image)
-            imgs[i,...] = torchvision.transforms.functional.to_tensor(timg)
-        return imgs[0]
-
-
+    def crop_and_resize(image, bounding_boxes, resize_shape):
+        imgs = []
+        for bb in bounding_boxes:
+            cropped_img = image.crop((bb[1], bb[0], bb[3], bb[2]))
+            resized_img = torchvision.transforms.Resize(resize_shape)(cropped_img)
+            imgs.append(torchvision.transforms.functional.to_tensor(resized_img))
+        imgs = torch.stack(imgs)
+        return imgs
 
     @staticmethod
     def get_bounding_boxes(filename):
@@ -145,112 +131,110 @@ class Dataset(torch.utils.data.Dataset):
         try:
             tree = ET.parse(filename)
             root = tree.getroot()
-
-            img_size = root.find('size')
-            img_width = int(img_size.find('width').text)
-            img_height = int(img_size.find('height').text)
-            # img_depth = int(img_size.find('depth').text)
-
             objs = root.findall('object')
-            bndbxs = []
-            # label = None
-            for object in objs:
-                # label = object.find('name').text
-                bndbox = object.find('bndbox')
-                bndbxs.append([
-                    int(bndbox.find('ymin').text),
-                    int(bndbox.find('xmin').text),
-                    int(bndbox.find('ymax').text),
-                    int(bndbox.find('xmax').text)
-                ])
-
-            return np.asarray(bndbxs,np.int)
+            bndbxs = [[int(obj.find('bndbox').find(dim).text) for dim in ['ymin', 'xmin', 'ymax', 'xmax']] for obj in objs]
+            return np.asarray(bndbxs, np.int64)
         except FileNotFoundError:
+            logger.debug(f'Annotation file not found: {filename}')
             return None
 
-
-
-
 def get_datasets(config):
-   
-
-   # this function creates the tf.dataset.Dataset objects for each list
-   # of input JPEGs.
-   train_ds = Dataset.from_filelist(config['data']['train_filelist'],config)
-   valid_ds = Dataset.from_filelist(config['data']['test_filelist'],config)
-
-   return train_ds,valid_ds
+    # this function creates the tf.dataset.Dataset objects for each list
+    # of input JPEGs.
+    train_ds = Dataset.from_filelist(config['data']['train_filelist'], config)
+    valid_ds = Dataset.from_filelist(config['data']['test_filelist'], config)
+    return train_ds, valid_ds
 
 
-if __name__ == '__main__':
-    # configure logging module
-    logging_format = '%(asctime)s %(levelname)s:%(process)s:%(thread)s:%(name)s:%(message)s'
-    logging_datefmt = '%Y-%m-%d %H:%M:%S'
-    logging_level = logging.INFO
-    logging.basicConfig(level=logging_level,
-                       format=logging_format,
-                       datefmt=logging_datefmt,
-                       stream = sys.stdout)
-    # parse command line
-    import argparse,json
-    parser = argparse.ArgumentParser(description='test this')
-    parser.add_argument('-c', '--config', dest='config_filename',
-                       help='configuration filename in json format',
-                       required=True)
-    parser.add_argument('-l','--logdir', dest='logdir',
-                       help='log output directory',default='logdir')
-    parser.add_argument('-n','--nsteps', dest='nsteps',
-                       help='number of steps to run',default=10,type=int)
-    parser.add_argument('-t', '--nthreads',help='number of threads, if set overrides config file',default=0,type=int)
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = os.environ['MASTER_ADDR']
+    os.environ['MASTER_PORT'] = os.environ['MASTER_PORT']
+
+    # initialize the process group
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
+def cleanup():
+    dist.destroy_process_group()
+
+def main():
+
+    DEFAULT_LOCAL_RANK = int(os.environ.get('PMI_LOCAL_RANK',0))
+    DEFAULT_LOCAL_SIZE = int(os.environ.get('PMI_LOCAL_SIZE',1))
+    DEFAULT_RANK = int(os.environ.get('PMI_RANK',0))
+    DEFAULT_SIZE = int(os.environ.get('PMI_SIZE',1))
+
+    print(f'local_rank: {DEFAULT_LOCAL_RANK}, local_size: {DEFAULT_LOCAL_SIZE}, rank: {DEFAULT_RANK}, size: {DEFAULT_SIZE}')
+
+    parser = ArgumentParser(description='PyTorch Dataset Example')
+    parser.add_argument('-c', '--config', dest='config_filename', required=True, help='Configuration filename in JSON format')
+    parser.add_argument('-l', '--logdir', dest='logdir', default='logdir', help='Log output directory')
+    parser.add_argument('-n', '--nsteps', dest='nsteps', default=10, type=int, help='Number of steps to run')
+    parser.add_argument('-t', '--nthreads', default=0, type=int, help='Number of threads, if set overrides config file')
+    parser.add_argument('--debug', action='store_true', help='Debug mode')
     args = parser.parse_args()
 
+
+    logging_format = '%(asctime)s %(levelname)s:%(process)s:%(thread)s:%(name)s:%(message)s'
+    logging_datefmt = '%Y-%m-%d %H:%M:%S'
+    logging.basicConfig(level=logging.INFO if not args.debug else logging.DEBUG,
+                        format=logging_format,
+                        datefmt=logging_datefmt,
+                        stream=sys.stdout)
+
     # parse config file
-    config = json.load(open(args.config_filename))
+    with open(args.config_filename) as f:
+        config = json.load(f)
+
     if args.nthreads > 0:
         config['data']['num_parallel_readers'] = args.nthreads
 
-    # logger.info('configruation file settings: \n %s',json.dumps(config,indent=4, sort_keys=True))
-
-    # these would be set by horovod if run in a parallel setup
-    rank = hvd.rank()
-    num_ranks = hvd.size()
+    setup(DEFAULT_RANK, DEFAULT_SIZE)
+    torch.cuda.set_device(DEFAULT_LOCAL_RANK)
+    device = torch.device(f'cuda:{DEFAULT_LOCAL_RANK}')
+    logger.info(f'Using device {device}')
 
     # call function to build dataset objects
     # both of the returned objects are tf.dataset.Dataset objects
     trainds, testds = get_datasets(config)
 
     ## create samplers for these datasets
-    train_sampler = torch.utils.data.distributed.DistributedSampler(trainds,num_ranks,rank,shuffle=True,drop_last=True)
-    test_sampler = torch.utils.data.distributed.DistributedSampler(testds,num_ranks,rank,shuffle=True,drop_last=True)
+    train_sampler = DistributedSampler(trainds, num_replicas=DEFAULT_SIZE, rank=DEFAULT_RANK, shuffle=True, drop_last=True)
+    test_sampler = DistributedSampler(testds, num_replicas=DEFAULT_SIZE, rank=DEFAULT_RANK, shuffle=True, drop_last=True)
 
-    ## create data loaders
     batch_size = config['data']['batch_size']
-    train_loader = torch.utils.data.DataLoader(trainds,shuffle=False,
-                                               sampler=train_sampler,num_workers=config['data']['num_parallel_readers'],
-                                               batch_size=batch_size,persistent_workers=True)
-    test_loader = torch.utils.data.DataLoader(testds,shuffle=False,
-                                               sampler=test_sampler,num_workers=config['data']['num_parallel_readers'],
-                                               batch_size=batch_size,persistent_workers=True)
+    train_loader = torch.utils.data.DataLoader(trainds, shuffle=False, sampler=train_sampler,
+                                               num_workers=config['data']['num_parallel_readers'],
+                                               batch_size=batch_size, persistent_workers=True)
+    test_loader = torch.utils.data.DataLoader(testds, shuffle=False, sampler=test_sampler,
+                                              num_workers=config['data']['num_parallel_readers'],
+                                              batch_size=batch_size, persistent_workers=True)
 
-    # epoch loop
+    # Assuming there is a model defined somewhere
+    # model = YourModel()
+    # model = model.to(device)
+    # model = DDP(model, device_ids=[args.local_rank], output_device=args.local_rank)
+
     for epoch in range(1):
-        if hvd.rank() == 0:
-            logger.info(f'epoch = {epoch}')
+        if DEFAULT_RANK == 0:
+            logger.info(f'Epoch: {epoch}')
 
         # calling this is required to get the shuffle to work and to reset the dataset 
         train_sampler.set_epoch(epoch)
-
         # can iterate over a dataset object
         start = time.time()
-        for batch_number,(inputs,labels) in enumerate(train_loader):
-            logger.info('batch_number = %s input shape = %s    labels shape = %s',batch_number,inputs.shape,labels.shape)
-            logger.info('batch_number = %s labels = %s',batch_number,np.squeeze(labels[0:10].numpy()).tolist())
-            
-            if batch_number > args.nsteps: break
-        duration = time.time() - start
-        images = config['data']['batch_size'] * args.nsteps
-        if hvd.rank() == 0:
-            logger.info('imgs/sec: %5.2f',images/duration*hvd.size())
-    
+        for batch_number, (inputs, labels) in enumerate(train_loader):
+            inputs, labels = inputs.to(device), labels.to(device)
+            logger.info(f'Batch {batch_number}: input shape = {inputs.shape}, labels shape = {labels.shape}')
+            logger.info(f'Batch {batch_number}: labels = {np.squeeze(labels[0:10].cpu().numpy()).tolist()}')
+            if batch_number > args.nsteps:
+                break
 
-    print('done',hvd.rank())
+        duration = time.time() - start
+        images_processed = batch_size * args.nsteps
+        if DEFAULT_RANK == 0:
+            logger.info(f'Images/sec/rank: {images_processed / duration :.2f}')
+
+    cleanup()
+
+if __name__ == '__main__':
+    main()
