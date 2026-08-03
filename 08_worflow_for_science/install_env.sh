a68819f39c106bbd0537c3fb17f2d300fb184ed9
Christine Simpson <csimpson@anl.gov>
2026-08-03 20:41:06 +0000

    adding env files

---
+++ 08_worflow_for_science/install_env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -l
+
+module load frameworks
+python -m venv _env --system-site-packages # including the --system-site-packages flag will inherit packages from frameworks
+pip install -r requirements
+dragon-config add --ofi-runtime-lib=/opt/cray/libfabric/1.22.0/lib64
